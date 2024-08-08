b88755075f8a7f2bf4cf0f83f1d2ddc65498a4d0
Natalia Ferreira <nataliaferreira.dev@yahoo.com>
2024-08-08 07:49:21 -0300

    Inclusao do Swagger

--- src/main/java/br/com/devsimples/tarefas/controller/TarefaController.java
+++ src/main/java/br/com/devsimples/tarefas/controller/TarefaController.java
@@ -2,6 +2,9 @@ package br.com.devsimples.tarefas.controller;
 
 import br.com.devsimples.tarefas.entity.TarefaDTO;
 import br.com.devsimples.tarefas.service.TarefaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
@@ -19,11 +22,17 @@ public class TarefaController {
         this.tarefaService = tarefaService;
     }
 
+    @Operation(summary = "Listar todas as tarefas")
     @GetMapping
     public List<TarefaDTO> listarTodos() {
         return tarefaService.listarTodas();
     }
 
+    @Operation(summary = "Buscar uma tarefa por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarefa encontrada"),
+            @ApiResponse(responseCode = "404", description = "Tarefa não encontrada")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<TarefaDTO> buscarPorId(@PathVariable Long id) {
         Optional<TarefaDTO> tarefaDTO = tarefaService.buscarPorId(id);
@@ -31,12 +40,14 @@ public class TarefaController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Criar uma nova tarefa")
     @PostMapping
     public ResponseEntity<TarefaDTO> criarTarefa(@Valid @RequestBody TarefaDTO tarefaDTO) {
         TarefaDTO novaTarefaDTO = tarefaService.criarTarefa(tarefaDTO);
         return ResponseEntity.ok(novaTarefaDTO);
     }
 
+    @Operation(summary = "Atualizar uma tarefa existente")
     @PutMapping("/{id}")
     public ResponseEntity<TarefaDTO> atualizarTarefa(@PathVariable Long id, @Valid @RequestBody TarefaDTO todoDTO) {
         Optional<TarefaDTO> updatedTodoDTO = tarefaService.atualizarTarefa(id, todoDTO);
@@ -44,12 +55,14 @@ public class TarefaController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Deletar uma tarefa por ID")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarTarefa(@PathVariable Long id) {
         boolean deletado = tarefaService.deletarTarefa(id);
         return deletado ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Marcar uma tarefa como finalizada")
     @PutMapping("/{id}/finalizar")
     public ResponseEntity<TarefaDTO> marcarComoFinalizado(@PathVariable Long id) {
         Optional<TarefaDTO> todoDTO = tarefaService.marcarComoFinalizado(id);
